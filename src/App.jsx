a0b4798802d0234b733d67536b306a2d839e68fb
Mykhailo Pylypiv <developer.mykhailo@gmail.com>
2025-06-13 05:46:02 -0600

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import { Route, Routes } from "react-router-dom";
 import Container from "./components/Container/Container";
+import HomePage from "./pages/HomePage";
+import MoviesPage from "./pages/MoviesPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import MovieDetailsPage from "./pages/MovieDetailsPage";
+import MovieCast from "./components/MovieCast/MovieCast";
+import MovieReviews from "./components/MovieReviews/MovieReviews";
 
 function App() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/movies" element={<MoviesPage />} />
+
+        <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+          <Route path="cast" element={<MovieCast />} />
+          <Route path="reviews" element={<MovieReviews />} />
+        </Route>
+
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Container>
+  );
 }
 
 export default App;
---
+++ src/components/MovieCast/MovieCast.jsx
@@ -0,0 +1,7 @@
+// import s from './MovieCast.module.css'
+
+const MovieCast = () => {
+  return <div>MovieCast</div>;
+};
+
+export default MovieCast;
