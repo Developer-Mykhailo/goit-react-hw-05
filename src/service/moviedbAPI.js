e4d898206665da63d559e5d5602effe27302d76d
Mykhailo Pylypiv <developer.mykhailo@gmail.com>
2025-06-14 07:40:30 -0600

    evn

--- src/service/moviedbAPI.js
+++ src/service/moviedbAPI.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
+const API_KEY = import.meta.evn.VITE_TMDB_API_KEY;
+
 axios.defaults.baseURL = "https://api.themoviedb.org/3/";
 axios.defaults.headers.common["accept"] = "application/json";
-axios.defaults.headers.common["Authorization"] =
-  "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NDI1ZmJmYWI0MjFjYjFjZGRkZWVjMjU5YWYwMGExMSIsIm5iZiI6MTc0OTg1OTYxNi4zOTUsInN1YiI6IjY4NGNiZDIwMzYyODg5NDA4MjNkZjMzNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.B4ystfuLacfyR5a0R4RoeE169M11LtF6137HWz4KImk";
+axios.defaults.headers.common["Authorization"] = API_KEY;
 
 export const getMovieAPI = async () => {
   const { data } = await axios.get("trending/movie/day", {
