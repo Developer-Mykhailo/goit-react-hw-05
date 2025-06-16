0b6dc096d06fcc3163761d592b3fa879be1750bd
Mykhailo Pylypiv <developer.mykhailo@gmail.com>
2025-06-15 21:05:48 -0600

    add useSearchParams

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,26 +1,43 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { getMovieByQueryAPI } from "../../service/moviedbAPI";
 import s from "./MoviesPage.module.css";
 import Container from "../../components/Container/Container";
 import MovieList from "../../components/MovieList/MovieList";
+import { useSearchParams } from "react-router-dom";
 
 const MoviesPage = () => {
   const [inputValue, setInputValue] = useState("");
   const [movieByQuery, setMovieByQuery] = useState([]);
-  //
+
+  const [searchParams, setSearchParams] = useSearchParams(); // access to url
+  const query = searchParams.get("query") ?? "";
 
   //hendlers
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!inputValue.trim()) return setInputValue("");
-    try {
-      const { results } = await getMovieByQueryAPI(inputValue);
-      setMovieByQuery(results);
-    } catch (error) {
-      console.log("Search failed:", error);
-    }
+
+    const nextSearchParams = new URLSearchParams(searchParams); // new instance of url
+    nextSearchParams.set("query", inputValue); // put new key with value
+    setSearchParams(nextSearchParams); // replace url
+
     setInputValue("");
   };
+
+  //
+
+  useEffect(() => {
+    const getMovieByQuery = async () => {
+      try {
+        const { results } = await getMovieByQueryAPI(query);
+        setMovieByQuery(results);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getMovieByQuery();
+  }, [query]);
 
   //JSX
   return (
